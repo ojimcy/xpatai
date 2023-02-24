a04e0820fb2ec8063b5cd1c8ddd835d855f1ef92
ojimcy <ojimcy247@gmail.com>
2023-02-24 16:57:41 +0100

    update slider, countdown and menu items

--- public/js/count-down.js
+++ public/js/count-down.js
@@ -78,7 +78,7 @@ Util.setAttributes = function (el, attrs) {
 
   CountDown.prototype.getEndTime = function () {
     // get number of remaining seconds
-    var end = new Date(2023, 2, 2); // March 2, 2023
+    var end = new Date(2023, 1, 28); // March 2, 2023
     return end.getTime();
   };
 
